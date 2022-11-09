25ba68fefee188db0a922774db9591f91370a782
Kingshuk Ghosh <kingshuk.ghosh@outlook.in>
2022-11-09 19:35:32 +0000

    second commit

--- src/kafka_config/__init__.py
+++ src/kafka_config/__init__.py
@@ -4,14 +4,17 @@ import os
 
 SECURITY_PROTOCOL="SASL_SSL"
 SSL_MACHENISM="PLAIN"
-API_KEY = os.getenv('API_KEY',None)
-ENDPOINT_SCHEMA_URL  = os.getenv('ENDPOINT_SCHEMA_URL',None)
-API_SECRET_KEY = os.getenv('API_SECRET_KEY',None)
-BOOTSTRAP_SERVER = os.getenv('BOOTSTRAP_SERVER',None)
+API_KEY = "WEMC6MAL3KMH7BLY"#os.getenv('API_KEY',None)
+ENDPOINT_SCHEMA_URL  = "https://psrc-xm8wx.eu-central-1.aws.confluent.cloud"#os.getenv('ENDPOINT_SCHEMA_URL',None)
+API_SECRET_KEY = "avIOqaRgGE1nLKJo4AUu33rPVm65pkjT9gRwdFPloFDSN92VNczoKo1nc6B7KmEH"
+#os.getenv('API_SECRET_KEY',None)
+BOOTSTRAP_SERVER = "pkc-41p56.asia-south1.gcp.confluent.cloud:9092"
+#os.getenv('BOOTSTRAP_SERVER',None)
 # SECURITY_PROTOCOL = os.getenv('SECURITY_PROTOCOL',None)
 # SSL_MACHENISM = os.getenv('SSL_MACHENISM',None)
-SCHEMA_REGISTRY_API_KEY = os.getenv('SCHEMA_REGISTRY_API_KEY',None)
-SCHEMA_REGISTRY_API_SECRET = os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
+SCHEMA_REGISTRY_API_KEY = "36NG5TRQQR5OLPUW"#os.getenv('SCHEMA_REGISTRY_API_KEY',None)
+SCHEMA_REGISTRY_API_SECRET = "lgIy+h9N8l4w/zTsNbYlJNrhB2reUSzhCPUFm8gJUtW0wuccHxPzUZ9zkW9QN0X3"
+#os.getenv('SCHEMA_REGISTRY_API_SECRET',None)
 
 
 def sasl_conf():
@@ -24,7 +27,7 @@ def sasl_conf():
                 'sasl.username': API_KEY,
                 'sasl.password': API_SECRET_KEY
                 }
-    print(sasl_conf)
+    #print(sasl_conf)
     return sasl_conf
 
 
